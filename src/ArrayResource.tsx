8175634678c541aaad70440629b6732fd18c644f
Andrew Chung <chung.andrew7@gmail.com>
2020-01-15 07:07:58 -0500

    bleehhhhh

---
+++ src/ArrayResource.tsx
@@ -0,0 +1,28 @@
+
+
+
+
+class ArrayResource extends Array {
+  #value;
+  constructor(...args) {
+    super(...args)
+    this.#value = args;
+
+    return new Proxy(this, {
+      get: (target, key, ...rest) => {
+        const keyIsIndex = typeof key === 'number' && key < this.length && key > 0;
+        const isThenable =  typeof this.#value[key].then === 'function';
+
+        // should all thenables be permitted, or only instances of Promise?
+        if(keyIsIndex && isThenable) {
+          throw this.#value[key];
+        }
+        return Reflect.get(target, key, ...rest);
+      }
+    });
+
+    
+  }
+}
+
+
