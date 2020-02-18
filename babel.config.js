74c79eb618c60eff4cd1c59f7b012f946e9ba815
Andrew Chung <chung.andrew7@gmail.com>
2020-02-17 21:42:28 -0500

    bleh

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-};
+  presets: [['@babel/preset-env', {       "useBuiltIns": "entry",
+  "modules": 'umd'}], '@babel/preset-react']
+}
