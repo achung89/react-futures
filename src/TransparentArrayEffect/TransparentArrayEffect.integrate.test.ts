e47a2e2e6e424242ed5816f0e03ef8af20c9d41c
Andrew Chung <chung.andrew7@gmail.com>
2020-02-21 00:25:53 -0500

    use suspense renderer in tests, change name of future array to TransparentArrayEffect

--- src/TransparentArrayEffect/TransparentArrayEffect.integrate.test.ts
+++ src/TransparentArrayEffect/TransparentArrayEffect.integrate.test.ts
@@ -2,7 +2,7 @@ import FutureArray from "./FutureArr";
 
 
 // test resetting value array prop with future array 
-/** obj.foo = new Array(() => obj.foo).map() */
+/** obj.foo = new Array(() => obj.foo).map() EDIT: will be taken cae of by oblique types*/
 // test suspending in same function as instantiation
 // test suspending in child 
 // testing iterating in parent and in child and accessing in child or subchild
