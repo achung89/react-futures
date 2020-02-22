d5b032ca32fd445975defae8a1f683668170fded
Andrew Chung <chung.andrew7@gmail.com>
2020-02-22 02:42:51 -0500

    add assertions for obj static methods

---
+++ src/FutureObject/FutureObj-statics.ts
@@ -0,0 +1,53 @@
+//FutureObject
+// TODO: add expected value
+export const futureObjectStatic = `       staticMethod         |   inRender                |  outRender                                                                                                                                                                               
+                              ${'assign'}                      |    ${'throw'}             |   ${'defermutate'}                                                 
+                              ${'getOwnPropertyDescriptor'}    |    ${'defer'}             |   ${'defer'}                                                             
+                              ${'getOwnPropertyDescriptors'}   |    ${'defer'}             |   ${'defer'}                                                                
+                              ${'getOwnPropertyNames'}         |    ${'defer'}             |   ${'defer'}                                                       
+                              ${'getOwnPropertySymbols'}       |    ${'defer'}             |   ${'defer'}                                                           
+                              ${'is'}                          |    ${'suspend'}           |   ${'throw'}                                        
+                              ${'preventExtensions'}           |    ${'throw'}             |   ${'defermutate'}                                                          
+                              ${'seal'}                        |    ${'throw'}             |   ${'defermutate'}                                         
+                              ${'create'}                      |    ${'throw'}             |   ${'throw'}                                            
+                              ${'defineProperties'}            |    ${'throw'}             |   ${'defermutate'}                                                     
+                              ${'defineProperty'}              |    ${'throw'}             |   ${'defermutate'}                                                 
+                              ${'freeze'}                      |    ${'throw'}             |   ${'defermutate'}                                          
+                              ${'getPrototypeOf'}              |    ${'suspend'}           |   ${'throw'}                                                     
+                              ${'setPrototypeOf'}              |    ${'throw'}             |   ${'defer'}                                                    
+                              ${'isExtensible'}                |    ${'suspend'}           |   ${'throw'}                                             
+                              ${'isFrozen'}                    |    ${'suspend'}           |   ${'throw'}                                         
+                              ${'isSealed'}                    |    ${'suspend'}           |   ${'throw'}                                              
+                              ${'keys'}                        |    ${'defer'}             |   ${'defer'}                                       
+                              ${'entries'}                     |    ${'defer'}             |   ${'defer'}                                                
+                              ${'values'}                      |    ${'defer'}             |   ${'defer'}                                      
+
+`
+
+
+//Object
+// autothrow means that it throws becuase of some other proxy handler
+// TODO: add expected value
+export const objectStatic = `       staticMethod         |   inRender                                           |  outRender                                                                                                                                                                               
+                        ${obj => Object.assign({},obj)}  |    ${'autothrow'}                                         |   ${'autothrow'}                                               
+                        ${'getOwnPropertyDescriptor'}    |    ${'suspend'}                                           |   ${'throw'}                                                             
+                        ${'getOwnPropertyDescriptors'}   |    ${'autosuspend'}                                       |   ${'throw'}                                                                
+                        ${'getOwnPropertyNames'}         |    ${'suspend'}                                           |   ${'defer'}                                                       
+                        ${'getOwnPropertySymbols'}       |    ${'return FutureObject.getOwnPropertyNames'}           |   ${'defer'}                                                           
+                        ${'is'}                          |    ${'none'}                                              |   ${'none'}                                        
+                        ${'preventExtensions'}           |    ${'throw'}                                             |   ${'defermutate'}                                                          
+                        ${'seal'}                        |    ${'autothrow'}                                         |   ${'autothrow'}                                         
+                        ${'create'}                      |    ${'suspend'}                                           |   ${'throw'}                                            
+                        ${'defineProperties'}            |    ${'throw'}                                             |   ${'autodefermutate'}                                                     
+                        ${'defineProperty'}              |    ${'throw'}                                             |   ${'defermutate'}                                                 
+                        ${'freeze'}                      |    ${'throw'}                                             |   ${'throw'}                                          
+                        ${'getPrototypeOf'}              |    ${'delegateToObject?'}                                  |   ${'delegateToObject?'}                                                     
+                        ${'setPrototypeOf'}              |    ${'throw'}                                             |   ${'throw? (show warning maybe)'}                                               
+                        ${'isExtensible'}                |    ${'suspend'}                                           |   ${'throw'}                                             
+                        ${'isFrozen'}                    |    ${'suspend'}                                           |   ${'throw'}                                         
+                        ${'isSealed'}                    |    ${'suspend'}                                           |   ${'throw'}                                              
+                        ${'keys'}                        |    ${'suspend'}                                           |   ${'throw'}                                       
+                        ${'entries'}                     |    ${'suspend'}                                           |   ${'throw'}                                                
+                        ${'values'}                      |    ${'suspend'}                                           |   ${'throw'}                                      
+
+`
--- src/FutureObject/FutureObj.unit.test.js
+++ src/FutureObject/FutureObj.unit.test.js
@@ -1,27 +1,86 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createFutureArray } from '../index';
+import { createFutureObject } from '../index';
 import { act } from 'react-dom/test-utils';
-
+import {objectStatic} from './FutureObj-statics';
 
 let Scheduler;
-const fetchArray = () => new Promise((res, rej) => {
+const fetchJson = val => new Promise((res, rej) => {
   setTimeout(() => {
-    res([2,3,4,5])
+    res({
+      foo: "futon",
+      bar: "barcandy",
+      bazz: "bazzerita",
+      value: val
+    })
   }, 1000)
 })
 
 let container;
-let FutureArr;
+const LogSuspense = ({ action }) => {
+  try {
+    action();
+    Scheduler.unstable_yieldValue('No Suspense');
+    return <span prop={text} />;
+  } catch (promise) {
+    if (typeof promise.then === 'function') {
+      Scheduler.unstable_yieldValue(`Suspend!`);
+    } else {
+      Scheduler.unstable_yieldValue(`Error!`);
+    }
+    throw promise;
+  }
+}
+
 beforeEach(() => {
-  FutureArr = createFutureArray(fetchArray);
+  FutureObj = createFutureObject(fetchJson);
   Scheduler = require('scheduler')
   container = document.createElement('div');
   document.body.appendChild(container);
 });
 afterEach(() => {
-  FutureArr = null;
+  FutureObj = null;
   document.body.removeChild(container);
   container = null;
 });
+
+describe("Object static methods behaviour", () => {
+  test.each`${objectStatic}`
+  (({staticMethod, inRender, outRender}) => {
+    test(`${typeof staticMethod === 'string' ? staticMethod : staticMethod.toString()} should ${inRender} inside render and ${outRender} outside render`, () => {
+      const futureObj = new FutureObj;
+      const method = typeof staticMethod === 'string' ? Object[staticMethod] : staticMethod;
+      const assertions = {
+        autothrow: () => expect(() => method(futureObj)).toThrowError(/** TODO: outofrender error */),
+        throw: () => expect(() => method(futureObj)).toThrowError(/** TODO: outofrender error */),
+        defer: () => expect(method(futureObj)).toBeInstanceOf(FutureObj),
+        defermutate: () => expect(method(futureObj)).toBeInstanceOf(FutureObj),
+        none: () => expect(() => method(futureObj)).not.toThrow(), 
+        suspend: () => {
+          method(futureObj)
+          expect(Scheduler).toFlushAndYieldThrough([
+           'Suspend!'
+          ])
+
+        },
+
+      }
+
+      assertions[outRender]();
+      
+      let App = () => {
+        return <LogSuspense action={() => assertions[inRender]()}>
+          <div>
+            foo
+        </div>
+        </LogSuspense>
+      };
+
+      act(() => {
+        ReactDOM.createRoot(container).render(<App />)
+      });
+    })
+  })
+})
+
