bf252ee85e1ef611dd438c6586050a6ff33d4335
Andrew Chung <chung.andrew7@gmail.com>
2020-03-26 00:14:23 -0400

    finish unit tests for Object.<static> method

--- src/FutureObject/FutureObj-statics.ts
+++ src/FutureObject/FutureObj-statics.ts
@@ -1,11 +1,23 @@
 import FutureObject from "./FutureObject"
+import FutureArray from "../FutureArray/FutureArray";
 
+const getOwnPropertyDescriptor = obj => Object.getOwnPropertyDescriptor(obj, 'foo')
+const assign_secondParam = obj => Object.assign({},obj);
+const defineProperty = obj => Object.defineProperty(obj, 'foo', {writable: false})
+const defineProperties = obj => Object.defineProperties(obj, {'foo':{ writable: false}})
+const setPrototypeOf = obj => Object.setPrototypeOf(obj, FutureArray)
+
+const getOwnPropertyDescriptorFuture = obj => FutureObject.getOwnPropertyDescriptor(obj, 'foo')
+const assign_secondParamFuture = obj => FutureObject.assign({},obj);
+const definePropertyFuture = obj => FutureObject.defineProperty(obj, 'foo', {writable: false})
+const definePropertiesFuture = obj => FutureObject.defineProperties(obj, {'foo':{ writable: false}})
+const setPrototypeOfFuture = obj => FutureObject.setPrototypeOf(obj, FutureArray)
 //FutureObject
 // TODO: add expected value
-export const eachFutureObjectStatic = test.each`
+export const eachFutureObjectStatic = test.only.each`
                                staticMethod                    |   inRender                |  outRender                                                                                                                                                                               
-${function assign(obj) { return FutureObject.assign({},obj)}}  |    ${'throw'}             |   ${'defermutate'}                                                 
-${'getOwnPropertyDescriptor'}                                  |    ${'defer'}             |   ${'defer'}                                                             
+${assign_secondParamFuture}                                    |    ${'throw'}             |   ${'defermutate'}                                                 
+${getOwnPropertyDescriptorFuture}                              |    ${'defer'}             |   ${'defer'}                                                             
 ${'getOwnPropertyDescriptors'}                                 |    ${'defer'}             |   ${'defer'}                                                                
 ${'getOwnPropertyNames'}                                       |    ${'defer'}             |   ${'defer'}                                                       
 ${'getOwnPropertySymbols'}                                     |    ${'defer'}             |   ${'defer'}                                                           
@@ -13,11 +25,11 @@ ${'is'}                                                        |    ${'suspend'}
 ${'preventExtensions'}                                         |    ${'throw'}             |   ${'defermutate'}                                                          
 ${'seal'}                                                      |    ${'throw'}             |   ${'defermutate'}                                         
 ${'create'}                                                    |    ${'throw'}             |   ${'throw'}                                            
-${'defineProperties'}                                          |    ${'throw'}             |   ${'defermutate'}                                                     
-${'defineProperty'}                                            |    ${'throw'}             |   ${'defermutate'}                                                 
+${definePropertiesFuture}                                          |    ${'throw'}             |   ${'defermutate'}                                                     
+${definePropertyFuture}                                            |    ${'throw'}             |   ${'defermutate'}                                                 
 ${'freeze'}                                                    |    ${'throw'}             |   ${'defermutate'}                                          
 ${'getPrototypeOf'}                                            |    ${'suspend'}           |   ${'throw'}                                                     
-${'setPrototypeOf'}                                            |    ${'throw'}             |   ${'defer'}                                                    
+${setPrototypeOfFuture}                                            |    ${'throw'}             |   ${'defer'}                                                    
 ${'isExtensible'}                                              |    ${'suspend'}           |   ${'throw'}                                             
 ${'isFrozen'}                                                  |    ${'suspend'}           |   ${'throw'}                                         
 ${'isSealed'}                                                  |    ${'suspend'}           |   ${'throw'}                                              
@@ -27,14 +39,11 @@ ${'values'}                                                    |    ${'defer'}  
 `
 
 
-//Object
+// Object
 // autothrow means that it throws becuase of some other proxy handler
 // TODO: add expected value
-function getOwnPropertyDescriptor(obj) { 
-       return Object.getOwnPropertyDescriptor(obj, 'foo')
-}
-function assign_secondParam(obj) { return Object.assign({},obj) };
-export const eachObjectStatic = test.only.each` 
+
+export const eachObjectStatic = test.each` 
 staticMethod                    |   inRender                                                |  outRender 
 ${assign_secondParam}           |    ${'suspend'}                                           |   ${'throw'}                                         
 ${getOwnPropertyDescriptor}     |    ${'suspend'}                                           |   ${'throw'}                                                          
@@ -42,18 +51,18 @@ ${'getOwnPropertyDescriptors'}  |    ${'suspend'}                               
 ${'getOwnPropertyNames'}        |    ${'suspend'}                                           |   ${'throw'}                                                       
 ${'getOwnPropertySymbols'}      |    ${'suspend'}                                           |   ${'throw'}                                                           
 ${'is'}                         |    ${'none'}                                              |   ${'none'}                                        
-${'preventExtensions'}          |    ${'throw'}                                             |   ${'throw'} `                                                         
-// `${'seal'}                       |    ${'autothrow'}                                         |   ${'autothrow'}                                         
-// ${'create'}                     |    ${'suspend'}                                           |   ${'throw'}                                            
-// ${'defineProperties'}           |    ${'throw'}                                             |   ${'autodefermutate'}                                                     
-// ${'defineProperty'}             |    ${'throw'}                                             |   ${'defermutate'}                                                 
-// ${'freeze'}                     |    ${'throw'}                                             |   ${'throw'}                                          
-// ${'getPrototypeOf'}             |    ${'delegateToObject?'}                                 |   ${'delegateToObject?'}                                                     
-// ${'setPrototypeOf'}             |    ${'throw'}                                             |   ${'throw? (show warning maybe)'}                                               
-// ${'isExtensible'}               |    ${'suspend'}                                           |   ${'throw'}                                             
-// ${'isFrozen'}                   |    ${'suspend'}                                           |   ${'throw'}                                         
-// ${'isSealed'}                   |    ${'suspend'}                                           |   ${'throw'}                                              
-// ${'keys'}                       |    ${'suspend'}                                           |   ${'throw'}                                       
-// ${'entries'}                    |    ${'suspend'}                                           |   ${'throw'}                                                
-// ${'values'}                     |    ${'suspend'}                                           |   ${'throw'}                                      
-// `
+${'preventExtensions'}          |    ${'throw'}                                             |   ${'defer'}                                                      
+${'seal'}                       |    ${'throw'}                                             |   ${'throw'}                                         
+${'create'}                     |    ${'none'}                                              |   ${'none'}                                          
+${defineProperties}             |    ${'throw'}                                             |   ${'defer'}                                                     
+${defineProperty}               |    ${'throw'}                                             |   ${'defer'}                                                 
+${'freeze'}                     |    ${'throw'}                                             |   ${'throw'}                                          
+${'getPrototypeOf'}             |    ${'none'}                                              |   ${'none'}                                                     
+${setPrototypeOf}               |    ${'throw'}                                             |   ${'none'}                                               
+${'isExtensible'}               |    ${'suspend'}                                           |   ${'throw'}                                             
+${'isFrozen'}                   |    ${'suspend'}                                           |   ${'throw'}                                         
+${'isSealed'}                   |    ${'suspend'}                                           |   ${'throw'}                                              
+${'keys'}                       |    ${'suspend'}                                           |   ${'throw'}                                       
+${'entries'}                    |    ${'suspend'}                                           |   ${'throw'}                                                
+${'values'}                     |    ${'suspend'}                                           |   ${'throw'}                                      
+`
