73df898e0cd0475382b79c4c8b44569755a2f22a
Andrew Chung <chung.andrew7@gmail.com>
2020-04-20 23:01:34 -0400

    completed integration object tests

--- src/FutureArray/__tests__/integrations/deep-render.test.tsx
+++ src/FutureArray/__tests__/integrations/deep-render.test.tsx
@@ -430,6 +430,7 @@ describe('Instantiate outside render, deep render scenario', () => {
 
 const createNestedFuture = numbers => {
   let numbers2 = new StubFutureArray(7); //[1,2,3,7];
+
   return numbers
     .map((num, ind) => num + numbers2[ind]) // [9,8,7,9]
     .filter(num =>
