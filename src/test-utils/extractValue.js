5131f308348a09577a4a1caafbbc27572dd208a9
Andrew Chung <chung.andrew7@gmail.com>
2020-04-21 21:10:17 -0400

    add promise implementation

--- src/test-utils/extractValue.js
+++ src/test-utils/extractValue.js
@@ -4,16 +4,23 @@ import React, {Suspense} from 'react';
 import { act } from 'react-dom/test-utils';
 const extractValue = future => {
   return new Promise((res, rej) => {
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    const App = () => {
-      res(getRaw(future))
-      return <div></div>
+    try {
+      const div = document.createElement('div');
+
+      document.body.appendChild(div);
+      const App = () => {
+        res(getRaw(future))
+        return <div></div>
+      }
+      act(() => {
+        render(<Suspense fallback={<div>Loading...</div>}><App /></Suspense>, div);
+      })
+      jest.runTimersToTime(0);
+    } catch (err) {
+      rej(err)
     }
-    act(() => {
-      render(<Suspense fallback={<div>Loading...</div>}><App /></Suspense>, div);
-    })
-    jest.runTimersToTime(0);
+
+    
   })
 }
 export default extractValue;
