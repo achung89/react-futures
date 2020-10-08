7dffdb2bf3a1ea7e0ead2ea896a75f36efd9aeab
Andrew Chung <chung.andrew7@gmail.com>
2020-10-07 21:13:00 -0400

    got all tests working with composed functor

--- src/PullCascade/PullCascade.ts
+++ src/PullCascade/PullCascade.ts
@@ -1,19 +1,20 @@
 import { first, tap } from "../utils"
 // TODO: test
 const PullCascade = cb => {
+  const firstCb = first(cb);
+
   return {
     get() {
-      return cb()
+      return firstCb()
     },
     map(fn) {
       //TODO: comment
-      const firstCb = first(cb);
-
+      
       return PullCascade(() => fn(firstCb()))
     },
     tap(fn) {
-
       //TODO: comment
+      
       return this.map(tap(fn));
     },
     get functor() {
